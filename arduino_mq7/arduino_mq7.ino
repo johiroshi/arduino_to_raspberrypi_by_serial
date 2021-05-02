0b33a5f8be9b692be49ab38f79eb2366ee38cfdb
ramda-phi <ad19880805@gmail.com>
2021-05-02 20:50:27 +0900

    initial commit

---
+++ arduino_mq7/arduino_mq7.ino
@@ -0,0 +1,24 @@
+#include "MQ7.h"
+
+const int AOUTpin=0;//the AOUT pin of the CO sensor goes into analog pin A0 of the arduino
+const int DOUTpin=8;//the DOUT pin of the CO sensor goes into digital pin D8 of the arduino
+
+MQ7 mq7(A0, 5.0);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);             // シリアル通信の準備をする
+  while (!Serial);                  // 準備が終わるのを待つ
+
+  pinMode(DOUTpin, INPUT);//sets the pin as an input to the arduino
+
+}
+
+void loop() {
+
+  Serial.print(mq7.getPPM());
+  Serial.println("");
+  delay(1000);
+
+}
